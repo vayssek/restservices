dd81618baf9da94e3270a3bcc992a91bc432765c
Kevin Vaysse <vayssek@gmail.com>
2017-07-19 08:46:22 -0500

    Update class Bookmark
    Adding member, getter and constructor

--- src/main/java/bookmarks/Bookmark.java
+++ src/main/java/bookmarks/Bookmark.java
@@ -1,5 +1,51 @@
 package bookmarks;
 
-public class Bookmark {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Bookmark {
+	
+	@JsonIgnore
+	@ManyToOne
+	private Account account;
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	public String uri;
+	public String description;
+	
+	Bookmark() {
+		// jpa only
+	}
+
+	public Bookmark(Account account, String uri, String description) {
+		super();
+		this.account = account;
+		this.uri = uri;
+		this.description = description;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
